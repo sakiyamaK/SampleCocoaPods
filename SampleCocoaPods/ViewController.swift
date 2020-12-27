c8b8607d40c2550dc4fd92324729c3e2b72902de
sakiyamaK <sakiyama.k@gmail.com>
2020-12-27 10:46:27 +0900

    fix

--- SampleCocoaPods/ViewController.swift
+++ SampleCocoaPods/ViewController.swift
@@ -10,28 +10,28 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
-    let protocolStr = "https"
-    let domain = "connpass.com"
-    let apiVersion = "v1"
-    let endpoint = "event"
-    var parameters: [String:String] = [:]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        parameters = [
-            "keyword_or": "東京"]
-
-        let urlStr = protocolStr + "://" + domain + "/api/" + apiVersion + "/" + endpoint
-        debugPrint(urlStr)
-        AF.request(urlStr, method: .get, parameters: parameters).response { (response) in
-            guard
-                let _data = response.data, let jsonStr = String(data: _data, encoding: .utf8) else {
-                    return
-            }
-            debugPrint(jsonStr)
-        }
+  
+  let protocolStr = "https"
+  let domain = "connpass.com"
+  let apiVersion = "v1"
+  let endpoint = "event"
+  var parameters: [String:String] = [:]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    parameters = [
+      "keyword_or": "東京"]
+    
+    let urlStr = protocolStr + "://" + domain + "/api/" + apiVersion + "/" + endpoint
+    debugPrint(urlStr)
+    AF.request(urlStr, method: .get, parameters: parameters).response { (response) in
+      guard
+        let _data = response.data, let jsonStr = String(data: _data, encoding: .utf8) else {
+        return
+      }
+      debugPrint(jsonStr)
     }
+  }
 }
 
